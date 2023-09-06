19353d1747187d0371d40d98fe72bc8917c9142c
vjspranav <pranavasri@live.in>
2023-09-06 15:28:37 +0530

    Add inital results

---
+++ post/ACME/Locust4/locust4.py
@@ -0,0 +1,68 @@
+import json
+import random
+import gevent
+import time
+import math
+from locust import HttpUser, task, between, LoadTestShape
+from gevent.lock import Semaphore
+from locust import events
+HEADER = {
+    'Content-Type': 'application/json;ty=4',
+    'Accept': 'application/json',
+    'X-M2M-Origin': 'Sacp-admin',
+    'X-M2M-RI': 'a2tzavpitws',
+    'X-M2M-RVI': '3'
+}
+
+
+class StepLoadShape(LoadTestShape):
+    step_time = 60
+    step_load = 5
+    spawn_rate = 10
+    time_limit = 7200
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time > self.time_limit:
+            return None
+
+        current_step = math.floor(run_time / self.step_time) + 1
+        return (current_step * self.step_load, self.spawn_rate)
+
+class MyUser(HttpUser):
+    wait_time = between(1, 15)
+    nodes_data = None
+
+    def on_start(self):
+        self.start_time = time.time()
+        self.all_nodes = []
+
+        with open('new_nodes.json') as f:
+            self.nodes_data = json.load(f)
+
+        for node_type, node_names in self.nodes_data.items():
+            self.all_nodes.extend(node_names)
+
+    @task
+    def increase_users(self):
+        user_num = self.environment.runner.user_count
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        print(f"User {user_num} - Time: {current_time}")
+
+        item = random.choice(self.all_nodes)
+        node_type = item.split('-')[0]
+        ri = self.nodes_data[node_type][item]
+        url = f"http://10.3.1.117:8002/" + ri
+
+        # Create the payload data for the POST request
+        payload = {
+            "m2m:cin": {
+                "cnf": "text/plain:0",
+                "con": "[1692945820, 190.00, 28.37, 66.2]"
+            }
+        }
+
+        # Send the POST request with the payload
+        self.client.post(url, headers=HEADER, json=payload)
+
---
+++ post/mobius/Locust4/locust4.py
@@ -0,0 +1,71 @@
+import json
+import random
+import gevent
+import time
+import math
+from locust import HttpUser, task, between, LoadTestShape
+from gevent.lock import Semaphore
+from locust import events
+HEADER = {
+    'X-M2M-Origin': 'SOrigin1',
+    'X-M2M-RI': '12345',
+    'Content-Type': 'application/json;ty=4;charset=utf-8'
+}
+
+class StepLoadShape(LoadTestShape):
+    step_time = 60
+    step_load = 5
+    spawn_rate = 10
+    time_limit = 7200
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time > self.time_limit:
+            return None
+
+        current_step = math.floor(run_time / self.step_time) + 1
+        return (current_step * self.step_load, self.spawn_rate)
+
+class MyUser(HttpUser):
+    wait_time = between(1, 15)
+    nodes_data = None
+
+    def on_start(self):
+        self.start_time = time.time()
+        self.all_nodes = []
+
+        with open('nodes.json') as f:
+            self.nodes_data = json.load(f)
+
+        for node_type, node_items in self.nodes_data.items():
+            for item in node_items:
+                self.all_nodes.append((node_type, item)) 
+    @task
+    def increase_users(self):
+
+        user_num = self.environment.runner.user_count
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Users {user_num} - Time: {current_time}")
+
+        
+        node_type, item = random.choice(self.all_nodes)
+        url = f"http://10.3.1.117:8001/Mobius/ae_{node_type}/{item}/Data"
+        
+        
+        # Create the payload data for the POST request
+        payload = {
+            "m2m:cin": {
+                "lbl": [
+                    "AE-AQ",
+                    "AQ-SN00-00",
+                    "V3.0.0",
+                    "AE-AQ-V3.0.0"
+                ],
+                "con": "[1692945820, 190.00, 28.37, 66.2]"
+            }
+        }
+        self.client.post(url, headers=HEADER, json=payload)
+
+ 
+
